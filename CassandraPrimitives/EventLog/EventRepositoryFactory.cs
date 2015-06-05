22012db5dfe2962913c3f96b5c39427ffc968b01
andrew <Andrew.Kostousov@gmail.com>
2015-06-05 16:45:03 +0500

    restore old RemoteLockCreator

--- CassandraPrimitives/EventLog/EventRepositoryFactory.cs
+++ CassandraPrimitives/EventLog/EventRepositoryFactory.cs
@@ -9,7 +9,6 @@ using SKBKontur.Catalogue.CassandraPrimitives.EventLog.Implementation;
 using SKBKontur.Catalogue.CassandraPrimitives.EventLog.Profiling;
 using SKBKontur.Catalogue.CassandraPrimitives.EventLog.Sharding;
 using SKBKontur.Catalogue.CassandraPrimitives.RemoteLock;
-using SKBKontur.Catalogue.CassandraPrimitives.RemoteLock.RemoteLocker;
 using SKBKontur.Catalogue.CassandraPrimitives.Storages.GlobalTicksHolder;
 
 namespace SKBKontur.Catalogue.CassandraPrimitives.EventLog
@@ -39,8 +38,8 @@ namespace SKBKontur.Catalogue.CassandraPrimitives.EventLog
             var globalTime = new GlobalTime(ticksHolder);
 
             var remoteLockImplementation = new CassandraRemoteLockImplementation(cassandraCluster, serializer, CassandraRemoteLockImplementationSettings.Default(columnFamilies.RemoteLock));
-            var remoteLocker = new RemoteLocker(remoteLockImplementation);
-            var eventLoggerAdditionalInfoRepository = new EventLoggerAdditionalInfoRepository(cassandraCluster, serializer, remoteLocker, columnFamilies.EventLogAdditionalInfo, columnFamilies.EventLog);
+            var remoteLockCreator = new RemoteLockCreator(remoteLockImplementation);
+            var eventLoggerAdditionalInfoRepository = new EventLoggerAdditionalInfoRepository(cassandraCluster, serializer, remoteLockCreator, columnFamilies.EventLogAdditionalInfo, columnFamilies.EventLog);
             var eventStorage = new EventStorage(columnFamilies.EventLog, eventLogPointerCreator, cassandraCluster, serializer);
             Func<IQueueRaker> createQueueRaker = () => new QueueRaker(eventStorage, eventLoggerAdditionalInfoRepository, eventInfoRepository, profiler);
             var eventLogger = new EventLogger(cassandraCluster, serializer, columnFamilies.EventLog, eventInfoRepository, eventLogPointerCreator, createQueueRaker, eventLoggerAdditionalInfoRepository, globalTime, profiler);
---
+++ CassandraPrimitives/RemoteLock/RemoteLockCreator.cs
@@ -0,0 +1,49 @@
+namespace SKBKontur.Catalogue.CassandraPrimitives.RemoteLock
+{
+    public class RemoteLockCreator : IRemoteLockCreator
+    {
+        public RemoteLockCreator(IRemoteLockImplementation remoteLockImplementation)
+        {
+            this.remoteLockImplementation = remoteLockImplementation;
+        }
+
+        public IRemoteLock Lock(string lockId)
+        {
+            return new RemoteLock(remoteLockImplementation, lockId);
+        }
+
+        public bool TryGetLock(string lockId, out IRemoteLock remoteLock)
+        {
+            string concurrentThreadId;
+            var weakRemoteLock = new WeakRemoteLock(remoteLockImplementation, lockId, out concurrentThreadId);
+            remoteLock = weakRemoteLock;
+            return string.IsNullOrEmpty(concurrentThreadId);
+        }
+
+        /// <summary>
+        ///     Метод только для целей тестирования, им не нужно пользоваться. Поэтому его нет в интерфейсе
+        /// </summary>
+        /// <param name="lockId"></param>
+        /// <returns></returns>
+        public IRemoteLock LockWithoutLocalRivalOptimization(string lockId)
+        {
+            return new RemoteLock(remoteLockImplementation, lockId, localRivalOptimization : false);
+        }
+
+        /// <summary>
+        ///     Метод только для целей тестирования, им не нужно пользоваться. Поэтому его нет в интерфейсе
+        /// </summary>
+        /// <param name="lockId"></param>
+        /// <param name="remoteLock"></param>
+        /// <returns></returns>
+        public bool TryGetLockWithoutLocalRivalOptimization(string lockId, out IRemoteLock remoteLock)
+        {
+            string concurrentThreadId;
+            var weakRemoteLock = new WeakRemoteLock(remoteLockImplementation, lockId, out concurrentThreadId, localRivalsOptimization : false);
+            remoteLock = weakRemoteLock;
+            return string.IsNullOrEmpty(concurrentThreadId);
+        }
+
+        private readonly IRemoteLockImplementation remoteLockImplementation;
+    }
+}
