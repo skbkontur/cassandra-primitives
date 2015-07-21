828727aa55c8247670e17051b3eacc2cbab05f1d
burmistrov <burmistrov@skbkontur.ru>
2015-07-21 21:05:29 +0500

    CassandraLockRepository: using GetColumns when read metadata instead of GetRow

--- CassandraPrimitives/RemoteLock/CassandraLockRepository.cs
+++ CassandraPrimitives/RemoteLock/CassandraLockRepository.cs
@@ -124,7 +124,7 @@ namespace SKBKontur.Catalogue.CassandraPrimitives.RemoteLock
             LockMetadata res = null;
             MakeInConnection(connection =>
                 {
-                    var columns = connection.GetRow(row).ToArray();
+                    var columns = connection.GetColumns(row, new []{"LockCount", "LockRowId"}).ToArray();
                     if(columns.Any(x => x.Name == "LockCount"))
                     {
                         res = new LockMetadata
