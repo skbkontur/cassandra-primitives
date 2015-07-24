267cc789e7bbdf75c4b0b30ba35f4873e15954d2
andrew <Andrew.Kostousov@gmail.com>
2015-07-24 12:57:49 +0500

    increase default KeepLockAliveInterval from 5 sec to 10 sec

--- CassandraPrimitives/RemoteLock/CassandraRemoteLockImplementationSettings.cs
+++ CassandraPrimitives/RemoteLock/CassandraRemoteLockImplementationSettings.cs
@@ -16,7 +16,7 @@ namespace SKBKontur.Catalogue.CassandraPrimitives.RemoteLock
                 {
                     ColumnFamilyFullName = columnFamilyFullName,
                     LockTtl = TimeSpan.FromMinutes(3),
-                    KeepLockAliveInterval = TimeSpan.FromSeconds(5),
+                    KeepLockAliveInterval = TimeSpan.FromSeconds(10),
                 };
         }
     }
