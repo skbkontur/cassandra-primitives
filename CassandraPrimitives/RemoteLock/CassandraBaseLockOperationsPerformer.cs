38c24037ca33ad83c053fc27a22da7f146a99a06
burmistrov <burmistrov@skbkontur.ru>
2015-08-05 17:40:06 +0500

    RemoteLock: CassandraBaseLockOperationsPerformer for "hard logic free" operations with Cassandra

---
+++ CassandraPrimitives/RemoteLock/CassandraBaseLockOperationsPerformer.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+using GroBuf;
+
+using SKBKontur.Cassandra.CassandraClient.Abstractions;
+using SKBKontur.Cassandra.CassandraClient.Clusters;
+using SKBKontur.Cassandra.CassandraClient.Connections;
+using SKBKontur.Catalogue.CassandraPrimitives.Storages.Primitives;
+
+namespace SKBKontur.Catalogue.CassandraPrimitives.RemoteLock
+{
+    internal class CassandraBaseLockOperationsPerformer
+    {
+        public CassandraBaseLockOperationsPerformer(ICassandraCluster cassandraCluster, ISerializer serializer, ColumnFamilyFullName columnFamilyFullName)
+        {
+            this.cassandraCluster = cassandraCluster;
+            this.serializer = serializer;
+            this.columnFamilyFullName = columnFamilyFullName;
+        }
+
+        public void WriteThread(string lockRowId, long? threshold, string threadId, TimeSpan ttl)
+        {
+            MakeInConnection(connection => connection.AddColumn(lockRowId, new Column
+                {
+                    Name = TransformThreadIdToColumnName(threshold, threadId),
+                    Value = new byte[] {0},
+                    Timestamp = GetNowTicks(),
+                    TTL = (int?)ttl.TotalSeconds
+                }));
+        }
+
+        public void DeleteThread(string lockRowId, long? threshold, string threadId)
+        {
+            MakeInConnection(connection => connection.DeleteBatch(lockRowId, new[] {TransformThreadIdToColumnName(threshold, threadId)}, GetNowTicks()));
+        }
+
+        public string[] SeatchThreads(string lockRowId, long? threshold)
+        {
+            var res = new string[0];
+            MakeInConnection(connection =>
+                {
+                    var columns = connection.GetRow(lockRowId, ThresholdToString(threshold)).ToArray();
+                    if(columns.Length != 0)
+                        res = columns.Where(x => x.Value != null && x.Value.Length != 0).Select(x => TransformColumnNameToThreadId(x.Name)).ToArray();
+                });
+            return res;
+        }
+
+        public void WriteLockMetadata(string lockMetadataRowId, LockMetadata lockMetadata)
+        {
+            var columns = new List<Column>
+                {
+                    new Column
+                        {
+                            Name = "LockRowId",
+                            Value = serializer.Serialize(lockMetadata.LockRowId),
+                            Timestamp = GetNowTicks()
+                        },
+                    new Column
+                        {
+                            Name = "LockCount",
+                            Value = serializer.Serialize(lockMetadata.LockCount),
+                            Timestamp = GetNowTicks()
+                        }
+                };
+            if(lockMetadata.PreviousLockOwner != null)
+            {
+                columns.Add(new Column
+                    {
+                        Name = "PreviousLockOwner",
+                        Value = serializer.Serialize(lockMetadata.PreviousLockOwner),
+                        Timestamp = GetNowTicks()
+                    });
+            }
+            if(lockMetadata.CurrentLockOwner != null)
+            {
+                columns.Add(new Column
+                    {
+                        Name = "CurrentLockOwner",
+                        Value = serializer.Serialize(lockMetadata.CurrentLockOwner),
+                        Timestamp = GetNowTicks()
+                    });
+            }
+
+            MakeInConnection(connection => connection.AddBatch(lockMetadataRowId, columns.ToArray()));
+        }
+
+        public LockMetadata GetLockMetadata(string lockMetadataRowId, string defaultLockRowId)
+        {
+            LockMetadata res = null;
+            MakeInConnection(connection =>
+                {
+                    var columns = connection.GetColumns(lockMetadataRowId, new[] {"LockCount", "LockRowId", "PreviousLockOwner", "CurrentLockOwner"});
+                    if(columns.Any(x => x.Name == "LockCount"))
+                    {
+                        res = new LockMetadata
+                            {
+                                LockCount = serializer.Deserialize<int>(columns.First(x => x.Name == "LockCount").Value),
+                                LockRowId = columns.Any(x => x.Name == "LockRowId")
+                                                ? serializer.Deserialize<string>(columns.First(x => x.Name == "LockRowId").Value)
+                                                : defaultLockRowId,
+                                PreviousLockOwner = columns.Any(x => x.Name == "PreviousLockOwner")
+                                                        ? serializer.Deserialize<LockOwner>(columns.First(x => x.Name == "PreviousLockOwner").Value)
+                                                        : null,
+                                CurrentLockOwner = columns.Any(x => x.Name == "CurrentLockOwner")
+                                                       ? serializer.Deserialize<LockOwner>(columns.First(x => x.Name == "CurrentLockOwner").Value)
+                                                       : null
+                            };
+                    }
+                });
+            return res;
+        }
+
+        private void MakeInConnection(Action<IColumnFamilyConnection> action)
+        {
+            var connection = cassandraCluster.RetrieveColumnFamilyConnection(columnFamilyFullName.KeyspaceName, columnFamilyFullName.ColumnFamilyName);
+            action(connection);
+        }
+
+        private long GetNowTicks()
+        {
+            var ticks = DateTime.UtcNow.Ticks;
+            while(true)
+            {
+                var last = Interlocked.Read(ref lastTicks);
+                var cur = Math.Max(ticks, last + 1);
+                if(Interlocked.CompareExchange(ref lastTicks, cur, last) == last)
+                    return cur;
+            }
+        }
+
+        private static string TransformThreadIdToColumnName(long? threshold, string threadId)
+        {
+            if(string.IsNullOrEmpty(threadId))
+                throw new ArgumentException("Empty ThreadId is not supported", "threadId");
+            if(threadId.Contains(delimiterSpecialSymbol))
+                throw new ArgumentException(string.Format("ThreadId cannot contains '{0}' symbol", delimiterSpecialSymbol), "threadId");
+            return threshold == null ? threadId : (ThresholdToString(threshold) + delimiterSpecialSymbol + threadId);
+        }
+
+        private static string ThresholdToString(long? threshold)
+        {
+            return threshold == null ? null : threshold.Value.ToString("D20");
+        }
+
+        private static string TransformColumnNameToThreadId(string columnName)
+        {
+            return columnName.Contains(delimiterSpecialSymbol) ? columnName.Split(delimiterSpecialSymbol)[1] : columnName;
+        }
+
+        private const char delimiterSpecialSymbol = ':';
+        private readonly ICassandraCluster cassandraCluster;
+        private readonly ISerializer serializer;
+        private readonly ColumnFamilyFullName columnFamilyFullName;
+        private long lastTicks;
+    }
+}
--- CassandraPrimitives/RemoteLock/CassandraLockRepository.cs
+++ CassandraPrimitives/RemoteLock/CassandraLockRepository.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 
 using GroBuf;
 
-using SKBKontur.Cassandra.CassandraClient.Abstractions;
 using SKBKontur.Cassandra.CassandraClient.Clusters;
-using SKBKontur.Cassandra.CassandraClient.Connections;
 using SKBKontur.Catalogue.CassandraPrimitives.Storages.Primitives;
 
 namespace SKBKontur.Catalogue.CassandraPrimitives.RemoteLock
@@ -16,29 +12,27 @@ namespace SKBKontur.Catalogue.CassandraPrimitives.RemoteLock
     {
         public CassandraLockRepository(ICassandraCluster cassandraCluster, ISerializer serializer, ColumnFamilyFullName columnFamilyFullName)
         {
-            this.cassandraCluster = cassandraCluster;
-            this.serializer = serializer;
-            this.columnFamilyFullName = columnFamilyFullName;
+            baseOperationsPerformer = new CassandraBaseLockOperationsPerformer(cassandraCluster, serializer, columnFamilyFullName);
         }
 
         public string[] GetThreadsInLockRow(LockMetadata lockMetadata)
         {
-            return SearchThreadsInRow(lockMetadata.PreviousLockOwner, GetMainRowKey(lockMetadata.LockRowId));
+            return baseOperationsPerformer.SeatchThreads(GetMainRowKey(lockMetadata.LockRowId), GetThreshold(lockMetadata.PreviousLockOwner));
         }
 
         public string[] GetShadowThreadsInLockRow(LockMetadata lockMetadata)
         {
-            return SearchThreadsInRow(lockMetadata.CurrentLockOwner, GetShadowRowKey(lockMetadata.LockRowId));
+            return baseOperationsPerformer.SeatchThreads(GetShadowRowKey(lockMetadata.LockRowId), GetThreshold(lockMetadata.CurrentLockOwner));
         }
 
         public void UnlockRow(LockMetadata lockMetadata, string threadId)
         {
-            DeleteThreadFromRow(lockMetadata.PreviousLockOwner, GetMainRowKey(lockMetadata.LockRowId), threadId);
+            baseOperationsPerformer.DeleteThread(GetMainRowKey(lockMetadata.LockRowId), GetThreshold(lockMetadata.PreviousLockOwner), threadId);
         }
 
         public void RelockRow(LockMetadata lockMetadata, string threadId, TimeSpan lockTtl)
         {
-            WriteThreadToRow(lockMetadata.PreviousLockOwner, GetMainRowKey(lockMetadata.LockRowId), threadId, lockTtl);
+            baseOperationsPerformer.WriteThread(GetMainRowKey(lockMetadata.LockRowId), GetThreshold(lockMetadata.PreviousLockOwner), threadId, lockTtl);
         }
 
         public LockAttemptResult TryLock(LockMetadata lockMetadata, string threadId, TimeSpan lockTtl, TimeSpan ttl)
@@ -76,71 +70,15 @@ namespace SKBKontur.Catalogue.CassandraPrimitives.RemoteLock
         {
             WriteThreadToRow(lockMetadata.CurrentLockOwner, GetMainRowKey(lockMetadata.LockRowId), threadId, ttl);
         }
-        
+
         public void WriteLockMetadata(string lockId, LockMetadata lockMetadata)
         {
-            var columns = new List<Column>
-                {
-                    new Column
-                        {
-                            Name = "LockRowId",
-                            Value = serializer.Serialize(lockMetadata.LockRowId),
-                            Timestamp = GetNowTicks()
-                        },
-                    new Column
-                        {
-                            Name = "LockCount",
-                            Value = serializer.Serialize(lockMetadata.LockCount),
-                            Timestamp = GetNowTicks()
-                        }
-                };
-            if(lockMetadata.PreviousLockOwner != null)
-            {
-                columns.Add(new Column
-                    {
-                        Name = "PreviousLockOwner",
-                        Value = serializer.Serialize(lockMetadata.PreviousLockOwner),
-                        Timestamp = GetNowTicks()
-                    });
-            }
-            if(lockMetadata.CurrentLockOwner != null)
-            {
-                columns.Add(new Column
-                    {
-                        Name = "CurrentLockOwner",
-                        Value = serializer.Serialize(lockMetadata.CurrentLockOwner),
-                        Timestamp = GetNowTicks()
-                    });
-            }
-
-            MakeInConnection(connection => connection.AddBatch(GetLockMetadataRowKey(lockId), columns.ToArray()));
+            baseOperationsPerformer.WriteLockMetadata(GetLockMetadataRowKey(lockId), lockMetadata);
         }
 
         public LockMetadata GetLockMetadata(string lockId, string defaultLockRowId)
         {
-            var row = GetLockMetadataRowKey(lockId);
-            LockMetadata res = null;
-            MakeInConnection(connection =>
-                {
-                    var columns = connection.GetColumns(row, new[] { "LockCount", "LockRowId", "PreviousLockOwner", "CurrentLockOwner" });
-                    if(columns.Any(x => x.Name == "LockCount"))
-                    {
-                        res = new LockMetadata
-                            {
-                                LockCount = serializer.Deserialize<int>(columns.First(x => x.Name == "LockCount").Value),
-                                LockRowId = columns.Any(x => x.Name == "LockRowId")
-                                                ? serializer.Deserialize<string>(columns.First(x => x.Name == "LockRowId").Value)
-                                                : defaultLockRowId,
-                                PreviousLockOwner = columns.Any(x => x.Name == "PreviousLockOwner")
-                                                ? serializer.Deserialize<LockOwner>(columns.First(x => x.Name == "PreviousLockOwner").Value)
-                                                : null,
-                                CurrentLockOwner = columns.Any(x => x.Name == "CurrentLockOwner")
-                                                ? serializer.Deserialize<LockOwner>(columns.First(x => x.Name == "CurrentLockOwner").Value)
-                                                : null
-                            };
-                    }
-                });
-            return res;
+            return baseOperationsPerformer.GetLockMetadata(GetLockMetadataRowKey(lockId), defaultLockRowId);
         }
 
         private static string GetShadowRowKey(string lockId)
@@ -160,74 +98,19 @@ namespace SKBKontur.Catalogue.CassandraPrimitives.RemoteLock
 
         private void WriteThreadToRow(LockOwner lockOwner, string rowName, string threadId, TimeSpan ttl)
         {
-            MakeInConnection(connection => connection.AddColumn(rowName, new Column
-                {
-                    Name = TransformThreadIdToColumnName(lockOwner, threadId),
-                    Value = new byte[] {0},
-                    Timestamp = GetNowTicks(),
-                    TTL = (int?)ttl.TotalSeconds
-                }));
-        }
-
-        private string[] SearchThreadsInRow(LockOwner lockOwner, string rowName)
-        {
-            var res = new string[0];
-            MakeInConnection(connection =>
-                {
-                    var columns = connection.GetRow(rowName, GetLockRowOperationsThreshold(lockOwner)).ToArray();
-                    if(columns.Length != 0)
-                        res = columns.Where(x => x.Value != null && x.Value.Length != 0).Select(x => TransformColumnNameToThreadId(x.Name)).ToArray();
-                });
-            return res;
-        }
-
-        private long GetNowTicks()
-        {
-            var ticks = DateTime.UtcNow.Ticks;
-            while(true)
-            {
-                var last = Interlocked.Read(ref lastTicks);
-                var cur = Math.Max(ticks, last + 1);
-                if(Interlocked.CompareExchange(ref lastTicks, cur, last) == last)
-                    return cur;
-            }
-        }
-
-        private void MakeInConnection(Action<IColumnFamilyConnection> action)
-        {
-            var connection = cassandraCluster.RetrieveColumnFamilyConnection(columnFamilyFullName.KeyspaceName, columnFamilyFullName.ColumnFamilyName);
-            action(connection);
+            baseOperationsPerformer.WriteThread(rowName, GetThreshold(lockOwner), threadId, ttl);
         }
 
         private void DeleteThreadFromRow(LockOwner lockOwner, string rowName, string threadId)
         {
-            MakeInConnection(connection => connection.DeleteBatch(rowName, new[] { TransformThreadIdToColumnName(lockOwner, threadId) }, GetNowTicks()));
+            baseOperationsPerformer.DeleteThread(rowName, GetThreshold(lockOwner), threadId);
         }
 
-        private string TransformThreadIdToColumnName(LockOwner lockOwner, string threadId)
+        private static long? GetThreshold(LockOwner lockOwner)
         {
-            if (string.IsNullOrEmpty(threadId))
-                throw new ArgumentException("Empty ThreadId is not supported", "threadId");
-            if (threadId.Contains(delimiterSpecialSymbol))
-                throw new ArgumentException(string.Format("ThreadId cannot contains '{0}' symbol", delimiterSpecialSymbol), "threadId");
-            var threshold = GetLockRowOperationsThreshold(lockOwner);
-            return threshold == null ? threadId : threshold + delimiterSpecialSymbol + threadId;
-       }
-
-        private string GetLockRowOperationsThreshold(LockOwner lockOwner)
-        {
-            return lockOwner == null ? null : lockOwner.LockRowThreshold.ToString();
+            return lockOwner == null ? (long?)null : lockOwner.LockRowThreshold;
         }
 
-        private string TransformColumnNameToThreadId(string columnName)
-        {
-            return columnName.Contains(delimiterSpecialSymbol) ? columnName.Split(delimiterSpecialSymbol)[1] : columnName;
-        }
-
-        private const char delimiterSpecialSymbol = ':';
-        private readonly ICassandraCluster cassandraCluster;
-        private readonly ISerializer serializer;
-        private readonly ColumnFamilyFullName columnFamilyFullName;
-        private long lastTicks;
+        private readonly CassandraBaseLockOperationsPerformer baseOperationsPerformer;
     }
 }
