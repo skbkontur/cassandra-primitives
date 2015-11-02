977d482f7e29ccac972f6a3b8a4ff1597e57faa8
andrew <Andrew.Kostousov@gmail.com>
2015-11-02 18:15:55 +0500

    fix timers in RemoteLockerMetrics to use Exponentially Decaying Reservoirs to monitor recent changes in quantiles of metrics

--- CassandraPrimitives/RemoteLock/RemoteLocker/RemoteLockerMetrics.cs
+++ CassandraPrimitives/RemoteLock/RemoteLocker/RemoteLockerMetrics.cs
@@ -9,14 +9,14 @@ namespace SKBKontur.Catalogue.CassandraPrimitives.RemoteLock.RemoteLocker
             Context = Metric.Context("RemoteLocker");
             if(!string.IsNullOrEmpty(keyspaceName))
                 Context = Context.Context(keyspaceName);
-            LockOp = Context.Timer("Lock", Unit.Calls, SamplingType.LongTerm, TimeUnit.Minutes);
-            TryGetLockOp = Context.Timer("TryGetLock", Unit.Calls, SamplingType.LongTerm, TimeUnit.Minutes);
-            TryAcquireLockOp = Context.Timer("TryAcquireLock", Unit.Calls, SamplingType.LongTerm, TimeUnit.Minutes);
-            ReleaseLockOp = Context.Timer("ReleaseLock", Unit.Calls, SamplingType.LongTerm, TimeUnit.Minutes);
-            KeepLockAliveOp = Context.Timer("KeepLock", Unit.Calls, SamplingType.LongTerm, TimeUnit.Minutes);
-            CassandraImplTryLockOp = Context.Timer("CassandraImpl.TryLock", Unit.Calls, SamplingType.LongTerm, TimeUnit.Minutes);
-            CassandraImplRelockOp = Context.Timer("CassandraImpl.Relock", Unit.Calls, SamplingType.LongTerm, TimeUnit.Minutes);
-            CassandraImplUnlockOp = Context.Timer("CassandraImpl.Unlock", Unit.Calls, SamplingType.LongTerm, TimeUnit.Minutes);
+            LockOp = Context.Timer("Lock", Unit.Calls, SamplingType.FavourRecent, TimeUnit.Minutes);
+            TryGetLockOp = Context.Timer("TryGetLock", Unit.Calls, SamplingType.FavourRecent, TimeUnit.Minutes);
+            TryAcquireLockOp = Context.Timer("TryAcquireLock", Unit.Calls, SamplingType.FavourRecent, TimeUnit.Minutes);
+            ReleaseLockOp = Context.Timer("ReleaseLock", Unit.Calls, SamplingType.FavourRecent, TimeUnit.Minutes);
+            KeepLockAliveOp = Context.Timer("KeepLock", Unit.Calls, SamplingType.FavourRecent, TimeUnit.Minutes);
+            CassandraImplTryLockOp = Context.Timer("CassandraImpl.TryLock", Unit.Calls, SamplingType.FavourRecent, TimeUnit.Minutes);
+            CassandraImplRelockOp = Context.Timer("CassandraImpl.Relock", Unit.Calls, SamplingType.FavourRecent, TimeUnit.Minutes);
+            CassandraImplUnlockOp = Context.Timer("CassandraImpl.Unlock", Unit.Calls, SamplingType.FavourRecent, TimeUnit.Minutes);
             FreezeEvents = Context.Meter("FreezeEvents", Unit.Events, TimeUnit.Hours);
         }
 
