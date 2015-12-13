19801613c9bfea2b0207501a57ad7d12206668e9
andrew <Andrew.Kostousov@gmail.com>
2015-12-13 17:55:21 +0500

    update cassandra client

--- CassandraPrimitives.Tests/FunctionalTests/Tests/RemoteLockTests/FailedCassandraCluster.cs
+++ CassandraPrimitives.Tests/FunctionalTests/Tests/RemoteLockTests/FailedCassandraCluster.cs
@@ -51,6 +51,11 @@ namespace SKBKontur.Catalogue.CassandraPrimitives.Tests.FunctionalTests.Tests.Re
             return CassandraCluster.RetrieveColumnFamilyConnection(keySpaceName, columnFamilyName);
         }
 
+        public IColumnFamilyConnectionImplementation RetrieveColumnFamilyConnectionImplementation(string keySpaceName, string columnFamilyName)
+        {
+            throw new NotImplementedException();
+        }
+
         public Dictionary<ConnectionPoolKey, KeyspaceConnectionPoolKnowledge> GetKnowledges()
         {
             return CassandraCluster.GetKnowledges();
