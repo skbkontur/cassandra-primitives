b491ca781ddea72a6eab017e0c4ad93712167a86
Пивко Максим Александрович <mpivko@kontur>
2016-07-06 15:23:10 +0500

    Do some more small fixes according to code review

--- CassandraPrimitives.Tests/RemoteLockBenchmark/TestRunner.cs
+++ CassandraPrimitives.Tests/RemoteLockBenchmark/TestRunner.cs
@@ -1,34 +1,67 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
+
+using log4net;
+
+using SKBKontur.Catalogue.TeamCity;
 
 namespace SKBKontur.Catalogue.CassandraPrimitives.Tests.RemoteLockBenchmark
 {
     public class TestRunner : ITestRunner
     {
         private readonly TestConfiguration configuration;
+        private readonly ITeamCityLogger teamCityLogger;
+        private readonly ILog logger;
 
-        public TestRunner(TestConfiguration configuration)
+        public TestRunner(TestConfiguration configuration, ITeamCityLogger teamCityLogger)
         {
             this.configuration = configuration;
+            this.teamCityLogger = teamCityLogger;
+            logger = LogManager.GetLogger(GetType());
         }
+
         public T RunTest<T>(ITest<T> test) where T : ITestResult
         {
             test.SetUp();
 
             var threads = new Thread[configuration.amountOfThreads];
-            for (int i = 0; i < configuration.amountOfThreads; i++)
+            for(int i = 0; i < configuration.amountOfThreads; i++)
             {
                 var threadInd = i;
-                threads[i] = new Thread(() => test.DoWorkInSingleThread(threadInd));
+                threads[i] = new Thread(() =>
+                    {
+                        try
+                        {
+                            test.DoWorkInSingleThread(threadInd);
+                        }
+                        catch(Exception e)
+                        {
+                            LogException("Exception occured in one of test threads", e);
+                        }
+                    });
             }
 
-            foreach (var thread in threads)
-                thread.Start();
-            foreach (var thread in threads)
-                thread.Join();//TODO timeout?
+            try
+            {
+                foreach(var thread in threads)
+                    thread.Start();
+                foreach(var thread in threads)
+                    thread.Join(); //TODO timeout?
+            }
+            catch (Exception e)
+            {
+                LogException("Exception occured while perfoming test", e);
+            }
 
             test.TearDown();
 
             return test.GetTestResult();
+        }
+
+        private void LogException(string description, Exception exception)
+        {
+            logger.Error(String.Format("{0}:", description), exception);
+            teamCityLogger.WriteMessageFormat(TeamCityMessageSeverity.Failure, "{0}:\n{1}", description, exception);
         }
 
         public void Dispose()
