102798830af5795631cbbe2eb3bfa4b8ff57dfda
Пивко Максим Александрович <mpivko@kontur>
2016-07-06 15:03:11 +0500

    Add TestRunner

---
+++ CassandraPrimitives.Tests/RemoteLockBenchmark/TestRunner.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+
+namespace SKBKontur.Catalogue.CassandraPrimitives.Tests.RemoteLockBenchmark
+{
+    public class TestRunner : ITestRunner
+    {
+        private readonly TestConfiguration configuration;
+
+        public TestRunner(TestConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public T RunTest<T>(ITest<T> test) where T : ITestResult
+        {
+            test.SetUp();
+
+            var threads = new Thread[configuration.amountOfThreads];
+            for (int i = 0; i < configuration.amountOfThreads; i++)
+            {
+                var threadInd = i;
+                threads[i] = new Thread(() => test.DoWorkInSingleThread(threadInd));
+            }
+
+            foreach (var thread in threads)
+                thread.Start();
+            foreach (var thread in threads)
+                thread.Join();//TODO timeout?
+
+            test.TearDown();
+
+            return test.GetTestResult();
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
