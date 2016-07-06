cc6a2e7b2bf6983241c65854eec1cd1bb30b79a9
andrew <Andrew.Kostousov@gmail.com>
2016-07-06 11:05:46 +0500

    + auto cleanup lock metadata after 30 days

--- CassandraPrimitives/RemoteLock/CassandraRemoteLockImplementationSettings.cs
+++ CassandraPrimitives/RemoteLock/CassandraRemoteLockImplementationSettings.cs
@@ -12,12 +12,14 @@ namespace SKBKontur.Catalogue.CassandraPrimitives.RemoteLock
             [NotNull] ITimestampProvider timestampProvider,
             [NotNull] ColumnFamilyFullName columnFamilyFullName,
             TimeSpan lockTtl,
+            TimeSpan lockMetadataTtl,
             TimeSpan keepLockAliveInterval,
             int changeLockRowThreshold)
         {
             TimestampProvider = timestampProvider;
             ColumnFamilyFullName = columnFamilyFullName;
             LockTtl = lockTtl;
+            LockMetadataTtl = lockMetadataTtl;
             KeepLockAliveInterval = keepLockAliveInterval;
             if(changeLockRowThreshold <= 0)
                 throw new ArgumentException("ChangeRowThreshold must be positive integer", "changeLockRowThreshold");
@@ -31,13 +33,14 @@ namespace SKBKontur.Catalogue.CassandraPrimitives.RemoteLock
         public ColumnFamilyFullName ColumnFamilyFullName { get; private set; }
 
         public TimeSpan LockTtl { get; private set; }
+        public TimeSpan LockMetadataTtl { get; private set; }
         public TimeSpan KeepLockAliveInterval { get; private set; }
         public int ChangeLockRowThreshold { get; private set; }
 
         [NotNull]
         public static CassandraRemoteLockImplementationSettings Default([NotNull] ColumnFamilyFullName columnFamilyFullName)
         {
-            return new CassandraRemoteLockImplementationSettings(new DefaultTimestampProvider(), columnFamilyFullName, TimeSpan.FromMinutes(3), TimeSpan.FromSeconds(10), 1000);
+            return new CassandraRemoteLockImplementationSettings(new DefaultTimestampProvider(), columnFamilyFullName, TimeSpan.FromMinutes(3), TimeSpan.FromDays(30), TimeSpan.FromSeconds(10), 1000);
         }
     }
 }
