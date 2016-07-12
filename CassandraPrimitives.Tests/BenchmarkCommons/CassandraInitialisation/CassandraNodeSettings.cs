e4e3468deca023a88d8be2c72b2d4967e158bce3
Пивко Максим Александрович <mpivko@kontur>
2016-07-12 17:12:54 +0500

    Add remote Cassandra launching

---
+++ CassandraPrimitives.Tests/BenchmarkCommons/CassandraInitialisation/CassandraNodeSettings.cs
@@ -0,0 +1,49 @@
+using System;
+using System.IO;
+
+namespace SKBKontur.Catalogue.CassandraPrimitives.Tests.BenchmarkCommons.CassandraInitialisation
+{
+    public class CassandraNodeSettings
+    {
+        public string Name { get; set; }
+        public int JmxPort { get; set; }
+        public int GossipPort { get; set; }
+        public int RpcPort { get; set; }
+        public int CqlPort { get; set; }
+        public string DataBaseDirectory { get; set; }
+        public string DeployDirectory { get; set; }
+        public string ListenAddress { get; set; }
+        public string RpsAddress { get; set; }
+        public string[] SeedAddresses { get; set; }
+        public string InitialToken { get; set; }
+        public string ClusterName { get; set; }
+
+        public CassandraNodeSettings(
+            string name = null,
+            int jmxPort = 7399,
+            int gossipPort = 7400,
+            int rpcPort = 9360,
+            int cqlPort = 9343,
+            string dataBaseDirectory = null,
+            string deployDirectory = null,
+            string listenAddress = null,
+            string rpsAddress = null,
+            string[] seedAddresses = null,
+            string initialToken = null,
+            string clusterName = null)
+        {
+            Name = name ?? "node_at_9360";
+            JmxPort = jmxPort;
+            GossipPort = gossipPort;
+            RpcPort = rpcPort;
+            CqlPort = cqlPort;
+            DataBaseDirectory = dataBaseDirectory ?? @"../data/";
+            DeployDirectory = deployDirectory ?? Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\Cassandra1.2");
+            ListenAddress = listenAddress ?? "127.0.0.1";
+            RpsAddress = rpsAddress ?? "0.0.0.0";
+            SeedAddresses = seedAddresses ?? new[] {"127.0.0.1"};
+            InitialToken = initialToken ?? "";
+            ClusterName = clusterName ?? "TestCluster";
+        }
+    }
+}
---
+++ CassandraPrimitives.Tests/BenchmarkCommons/RemoteTaskRunning/TaskSchedulerAdapter.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+using log4net;
+
+using Microsoft.Win32.TaskScheduler;
+
+namespace SKBKontur.Catalogue.CassandraPrimitives.Tests.BenchmarkCommons.RemoteTaskRunning
+{
+    public class TaskSchedulerAdapter : IDisposable
+    {
+        public TaskSchedulerAdapter(RemoteTaskSchedulerCredentials credentials)
+        {
+            taskService = credentials == null ? new TaskService() : new TaskService(credentials.MachineName, credentials.UserName, credentials.AccountDomain, credentials.Password);
+            this.credentials = credentials;
+            logger = LogManager.GetLogger(GetType());
+        }
+
+        public Task RunTask(string taskName, string path, string[] arguments = null, string directory = null)
+        {
+            var argsRepresentation = string.Format("[{0}]", String.Join(", ", arguments ?? new string[0]));
+            logger.InfoFormat("Scheduling task. Path = {0}, name = {1}, arguments = {2}, machine = {3}", path, taskName, argsRepresentation, credentials.MachineName);
+
+            var existingTask = taskService.FindTask(taskName);
+            if (existingTask != null)
+            {
+                logger.InfoFormat("Found existing task with name {0}", taskName);
+                existingTask.Stop();
+                taskService.RootFolder.DeleteTask(taskName);
+                logger.InfoFormat("Existing task with name {0} successfully stopped and deleted", taskName);
+            }
+            else
+                logger.InfoFormat("Existing task with name {0} not found", taskName);
+
+            try
+            {
+                Task task = CreateTask(taskName, path, arguments, directory);
+                task.Run();
+                if (!IsTaskRunning(task, taskStartTimeoutMilliseconds))
+                {
+                    logger.ErrorFormat("The task with name {0} on machine {1} didn't start", taskName, credentials.MachineName);
+                    throw new Exception("Fail staring task");
+                }
+                logger.InfoFormat("The task with name {0} on machine {1} successfully started", taskName, credentials.MachineName);
+                return task;
+            }
+            catch (COMException e)
+            {
+                logger.ErrorFormat("ErrorCode: {0}", e.ErrorCode);
+                if (e.ErrorCode == -2147023108)
+                    throw new Exception(String.Format("Unknown user {0}", credentials.UserName));
+                if (e.ErrorCode == -2147023570)
+                    throw new Exception(String.Format("Invalid user or password (username - {0})", credentials.UserName));
+                throw;
+            }
+        }
+
+        private bool IsTaskRunning(Task task, int timeoutMilliseconds)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            while (task.State != TaskState.Running && stopwatch.ElapsedMilliseconds < timeoutMilliseconds)
+            {
+                Thread.Sleep(100);
+                return false;
+            }
+            return true;
+        }
+
+        private Task CreateTask(string taskName, string path, string[] arguments = null, string directory = null)
+        {
+            path = String.Format("\"{0}\"", path);
+
+            var taskDefinition = taskService.NewTask();
+
+            var joinedArgs = String.Join(" ", (arguments ?? new string[0]).Select(EscapeCmdArgument));
+
+            var action = new ExecAction(path, joinedArgs, directory);
+
+            taskDefinition.Actions.Add(action);
+            taskDefinition.Triggers.Add(new TimeTrigger());
+            taskDefinition.Principal.RunLevel = TaskRunLevel.Highest;
+            taskDefinition.Principal.LogonType = TaskLogonType.Password;
+            taskDefinition.Settings.AllowDemandStart = true;
+            taskDefinition.Settings.StartWhenAvailable = true;
+            taskDefinition.Settings.AllowHardTerminate = true;
+            taskDefinition.Settings.ExecutionTimeLimit = TimeSpan.Zero;
+            taskDefinition.Settings.Priority = ProcessPriorityClass.High;
+            var task = taskService.RootFolder.RegisterTaskDefinition(taskName, taskDefinition, TaskCreation.CreateOrUpdate, "SYSTEM", null, TaskLogonType.Password);
+
+            return task;
+        }
+
+        public Task RunTaskInWrapper(string taskName, string path, string[] arguments = null, string directory = null)
+        {
+            return RunTask(taskName, wrapperPath, new[] {"Normal", path}.Concat(arguments ?? new string[0]).ToArray(), directory);
+        }
+
+        private string EscapeCmdArgument(string argument)
+        {
+            var r = new Regex(@"\\(?=""|$)");
+            var semiEscaped = r.Replace(argument, @"\\\\");
+            var result = String.Format("\"{0}\"", semiEscaped.Replace(@"""", @""""""""""));
+            return result;
+        }
+
+        public void Dispose()
+        {
+            taskService.Dispose();
+        }
+
+        private const int taskStartTimeoutMilliseconds = 3000;
+        //TODO: replace absolute path with relative one
+        private const string wrapperPath = @"C:\workspace\tmp\MoreJobObjects\MoreJobObjects\bin\Release\MoreJobObjects.exe";
+
+        private readonly TaskService taskService;
+        private readonly ILog logger;
+        private readonly RemoteTaskSchedulerCredentials credentials;
+    }
+}
