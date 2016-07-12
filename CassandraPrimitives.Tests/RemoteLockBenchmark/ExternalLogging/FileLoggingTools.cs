e2f17c71709340cd65083f0d8c3cf6afd5a28e62
Пивко Максим Александрович <mpivko@kontur>
2016-07-12 17:49:58 +0500

    External logging through files

---
+++ CassandraPrimitives.Tests/RemoteLockBenchmark/ExternalLogging/FileLoggingTools.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace SKBKontur.Catalogue.CassandraPrimitives.Tests.RemoteLockBenchmark.ExternalLogging
+{
+    public class FileLoggingTools
+    {
+        public static string CreateLogFileAndGetPath(string workingDirectory, int processInd)
+        {
+            var logDir = Path.Combine(workingDirectory, "ExternalLogging");
+            if (!Directory.Exists(logDir))
+                Directory.CreateDirectory(logDir);
+            var filename = String.Format("log_proc_{0}.txt", processInd);
+            return Path.Combine(logDir, filename);
+        }
+
+        public static string GetLogFilePath(string workingDirectory, int processInd)
+        {
+            var logDir = Path.Combine(workingDirectory, "ExternalLogging");
+            if (!Directory.Exists(logDir))
+                throw new Exception(String.Format("External logs directory ({0}) doesn't exist", logDir));
+            var filename = String.Format("log_proc_{0}.txt", processInd);
+            return Path.Combine(logDir, filename);
+        }
+    }
+}
