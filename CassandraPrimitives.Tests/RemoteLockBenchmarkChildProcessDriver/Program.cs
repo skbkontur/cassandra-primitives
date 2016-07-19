0580f1617cae6bd73331fb464ef95a483bca637c
Пивко Максим Александрович <mpivko@kontur>
2016-07-19 18:02:19 +0500

    Better logging

--- CassandraPrimitives.Tests/RemoteLockBenchmarkChildProcessDriver/Program.cs
+++ CassandraPrimitives.Tests/RemoteLockBenchmarkChildProcessDriver/Program.cs
@@ -1,27 +1,45 @@
 ﻿using System;
 using System.IO;
 
+using log4net;
+
+using SKBKontur.Catalogue.CassandraPrimitives.Tests.RemoteLockBenchmark.Logging;
 using SKBKontur.Catalogue.CassandraPrimitives.Tests.RemoteLockBenchmark.TestConfigurations;
 
 namespace SKBKontur.Catalogue.CassandraPrimitives.Tests.RemoteLockBenchmarkChildProcessDriver
 {
     internal class Program
     {
+        private static ILog logger;
+
         private static void Main(string[] args)
         {
+            Log4NetConfiguration.InitializeOnce();
+            logger = LogManager.GetLogger(typeof(Program));
+
             AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
-            int threadInd;
-            if (!int.TryParse(args[0], out threadInd))
+            int processInd;
+            if (!int.TryParse(args[0], out processInd))
+            {
                 Console.WriteLine("Invalid argument");
+                logger.ErrorFormat("Invalid process id {0}", args[0]);
+            }
+
+            logger.InfoFormat("Process id is {0}", processInd);
+            logger.InfoFormat("Remote http address is {0}", args[1]);
+
             TestConfiguration configuration;
             using (var httpExternalDataProvider = new HttpExternalDataGetter(args[1]))
                 configuration = httpExternalDataProvider.GetTestConfiguration().Result;
-            ChildProcessDriver.RunSingleTest(threadInd, configuration, AppDomain.CurrentDomain.BaseDirectory);
+
+            logger.InfoFormat("Configuration was received");
+            
+            ChildProcessDriver.RunSingleTest(processInd, configuration, AppDomain.CurrentDomain.BaseDirectory);
         }
 
         private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
-            File.AppendAllText("C:\\errors.txt", e.ExceptionObject.ToString());
+            logger.Error(e.ExceptionObject.ToString());
         }
     }
 }
