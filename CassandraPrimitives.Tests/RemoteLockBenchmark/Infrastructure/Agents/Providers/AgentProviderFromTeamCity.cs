a6579758e84b5a6e976acc456a068c2c6b367d98
Пивко Максим Александрович <mpivko@kontur>
2016-07-27 13:20:08 +0500

    Change format of agents parameter

--- CassandraPrimitives.Tests/RemoteLockBenchmark/Infrastructure/Agents/Providers/AgentProviderFromTeamCity.cs
+++ CassandraPrimitives.Tests/RemoteLockBenchmark/Infrastructure/Agents/Providers/AgentProviderFromTeamCity.cs
@@ -8,16 +8,11 @@ namespace SKBKontur.Catalogue.CassandraPrimitives.Tests.RemoteLockBenchmark.Infr
     {
         internal override List<string> GetAllAgentNames()
         {
-            int amount;
-            if (!int.TryParse(Environment.GetEnvironmentVariable("benchmark.AmountOfAgents"), out amount))
-                throw new Exception("Invalid value was given for parameter benchmark.AmountOfAgents");
+            var rawAgents = Environment.GetEnvironmentVariable("benchmark.Agents");
+            if (rawAgents == null)
+                throw new Exception("No value was given for parameter benchmark.Agents");
 
-            var agentNames = Enumerable
-                .Range(0, amount)
-                .Select(i => Environment.GetEnvironmentVariable(string.Format("benchmark.Agent{0}", i)))
-                .ToList();
-            if (agentNames.Any(string.IsNullOrEmpty))
-                throw new Exception(string.Format("Null or empty agent name was given for agent with id {0}", agentNames.FindIndex(string.IsNullOrEmpty)));
+            var agentNames = rawAgents.Split('|').ToList();
 
             return agentNames;
         }
