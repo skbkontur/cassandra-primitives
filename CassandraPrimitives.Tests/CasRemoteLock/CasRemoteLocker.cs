82b77698eaf5eeb700cca5d5a639abf21bf21353
mpivko <mpivko@skbkontur.ru>
2016-08-23 19:01:12 +0500

    Just ignore table already exists exception

--- CassandraPrimitives.Tests/CasRemoteLock/CasRemoteLocker.cs
+++ CassandraPrimitives.Tests/CasRemoteLock/CasRemoteLocker.cs
@@ -39,12 +39,19 @@ namespace SKBKontur.Catalogue.CassandraPrimitives.Tests.CasRemoteLock
         public void ActualiseTables()
         {
             //session.Execute(string.Format("DROP TABLE \"{0}\";", tableName));//TODO
-            if (session.Execute("SELECT * FROM system.schema_columnfamilies;").Any(row => row.GetValue<string>("columnfamily_name") == tableName))
-                return;
-            session.Execute(string.Format("CREATE TABLE \"{0}\" (", tableName) +
-                            "lock_id text PRIMARY KEY," +
-                            "owner text," +
-                            ");");
+            //if (session.Execute("SELECT * FROM system.schema_columnfamilies;").Any(row => row.GetValue<string>("columnfamily_name") == tableName))
+            //    return;
+            try
+            {
+                session.Execute(string.Format("CREATE TABLE \"{0}\" (", tableName) +
+                                            "lock_id text PRIMARY KEY," +
+                                            "owner text," +
+                                            ");");
+            }
+            catch (Exception)
+            {
+                //ignore
+            }
         }
 
         public bool TryAcquire(string lockId, out IDisposable releaser)
