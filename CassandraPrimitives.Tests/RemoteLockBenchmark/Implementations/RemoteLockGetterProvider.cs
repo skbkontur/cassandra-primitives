8731e37b9634674c2cd6c12e13b3455e4c13eea9
mpivko <mpivko@skbkontur.ru>
2016-08-24 14:20:20 +0500

    Create single CassandraCasRemoteLockGetter

--- CassandraPrimitives.Tests/RemoteLockBenchmark/Implementations/RemoteLockGetterProvider.cs
+++ CassandraPrimitives.Tests/RemoteLockBenchmark/Implementations/RemoteLockGetterProvider.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 
 using SKBKontur.Catalogue.CassandraPrimitives.Tests.BenchmarksInfrastructure.Infrastructure.ExternalLogging;
 using SKBKontur.Catalogue.CassandraPrimitives.Tests.BenchmarksInfrastructure.Infrastructure.ExternalLogging.HttpLogging;
@@ -15,24 +16,33 @@ namespace SKBKontur.Catalogue.CassandraPrimitives.Tests.RemoteLockBenchmark.Impl
         {
             switch (configuration.ClusterType)
             {
-            case ClusterTypes.Cassandra:
-            case ClusterTypes.DeployedCassandra:
-                var cassandraClusterSettings = httpExternalDataGetter.GetCassandraSettings().Result;
-                getter = () => new CassandraRemoteLockGetter(cassandraClusterSettings);
-                break;
-            case ClusterTypes.CassandraCas:
-                cassandraClusterSettings = httpExternalDataGetter.GetCassandraSettings().Result;
-                getter = () => new CassandraCasRemoteLockGetter(cassandraClusterSettings);
-                break;
-            case ClusterTypes.Zookeeper:
-            case ClusterTypes.DeployedZookeeper:
-                var zookeeperClusterSettings = httpExternalDataGetter.GetZookeeperSettings().Result;
-                getter = () => new ZookeeperRemoteLockGetter(new ZookeeperLockSettings(zookeeperClusterSettings.ConnectionString, "/RemoteLockBenchmark", TimeSpan.FromSeconds(100)));
-                break;
-            default:
-                throw new Exception(string.Format("Unknown remote lock implementation {0}", configuration.ClusterType));
+                case ClusterTypes.Cassandra:
+                    var cassandraClusterSettings = httpExternalDataGetter.GetCassandraSettings().Result;
+                    getter = () => new CassandraRemoteLockGetter(cassandraClusterSettings);
+                    break;
+                case ClusterTypes.CassandraCas:
+                    cassandraClusterSettings = httpExternalDataGetter.GetCassandraSettings().Result;
+                    getter = () =>
+                    {
+                        lock (cassandraClusterSettings)
+                        {
+                            if (cassandraCasRemoteLockGetter == null)
+                                cassandraCasRemoteLockGetter = new CassandraCasRemoteLockGetter(cassandraClusterSettings);
+                        }
+                        return cassandraCasRemoteLockGetter;
+                    };
+                    break;
+                case ClusterTypes.Zookeeper:
+                    var zookeeperClusterSettings = httpExternalDataGetter.GetZookeeperSettings().Result;
+                    getter = () => new ZookeeperRemoteLockGetter(new ZookeeperLockSettings(zookeeperClusterSettings.ConnectionString, "/RemoteLockBenchmark", TimeSpan.FromSeconds(100)));
+                    break;
+                default:
+                    throw new Exception(string.Format("Unknown remote lock implementation {0}", configuration.ClusterType));
+
             }
         }
+
+        private CassandraCasRemoteLockGetter cassandraCasRemoteLockGetter;
 
         public IRemoteLockGetter GetRemoteLockGetter()
         {
