e40466a3bb36a6e58e50b056b667964eabc37b39
avk <Andrew.Kostousov@gmail.com>
2018-06-04 13:42:57 +0500

    use ContextLogWrapper

--- CassandraPrimitives/EventLog/EventRepositoryFactory.cs
+++ CassandraPrimitives/EventLog/EventRepositoryFactory.cs
@@ -28,7 +28,7 @@ namespace SKBKontur.Catalogue.CassandraPrimitives.EventLog
             this.serializer = serializer;
             this.cassandraCluster = cassandraCluster;
             this.eventTypeIdentifierProvider = eventTypeIdentifierProvider;
-            this.logger = logger;
+            this.logger = logger.WithContext("CassandraPrimitives.EventLog");
         }
 
         public IEventRepository CreateEventRepository(IShardCalculator shardCalculator, IEventRepositoryColumnFamilyFullNames columnFamilies)
